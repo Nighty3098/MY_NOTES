3eaa1523dae74533c5788f5820f4872c8428a54e
Nighty3098 <night3098games@gmail.com>
2025-03-26 23:40:22 +0700

    2025-03-26 23:40:22 PUSH BY Nighty3098WS

--- sync.sh
+++ sync.sh
@@ -5,10 +5,14 @@ device_name=$(hostname)
 
 echo -e "Starting ..."
 
+cat TOKEN.md
+
 git pull
 
 git add .
 git commit -m "${current_date} PUSH BY ${device_name}"
+
+cat TOKEN.md
 git push
 
 git pull
